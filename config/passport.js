901384c6134134f7463cf82a70bc4324ea36cbe1
Jossyelmos <jossyelmos@yahoo.com>
2026-04-10 16:20:45 +0100

    updated

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/github/callback"
+      callbackURL: process.env.GITHUB_CALLBACK_URL
     },
     function (accessToken, refreshToken, profile, done) {
       // You can save user to DB here later
